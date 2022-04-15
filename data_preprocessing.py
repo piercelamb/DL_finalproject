a3150bf5b71572dafd04265fc9742e8001dad54f
Pierce Lamb <plamb@visotrust.com>
2022-04-15 07:51:03 -0700

    minor changes to data preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,14 +1,18 @@
 import os
 import pandas as pd
 import re
+from datasets import Dataset, Features
 
 DEEPMIND_MATH_PATH = './data/mathematics_dataset-v1.0'
+SAVED_DATA_PATH = './data/'
 
+FAIRSEQ_MODEL_PATH = './models'
 
-def get_labeled_data():
-    # Possible versions ' 13 ', ' 13,', '8*13', ' 13.2008'
-    # TODO update removed numbers with all possibilities
-    # TODO may need a regex that matches ' 13 ', ' 13,', '8*13', ' 13.2008' but not '11113111'
+def reduce_dataset():
+    if os.path.isfile(SAVED_DATA_PATH+'dataset.csv'):
+        print("Detected filtered data locally, returning")
+        return pd.read_csv(SAVED_DATA_PATH+'dataset.csv')
+
     removed_numbers_ints = [' 13 ', ' 31 ', ' 82 ', ' 99 ']
     removed_numbers = ["\D1[3]{1}\D", "\D3[1]{1}\D", "\D8[2]{1}\D", "\D9[9]{1}\D"]
     print("Getting arthimetic data and filtering out any instances with the following numbers: " + ', '.join(
@@ -25,25 +29,30 @@ def get_labeled_data():
                     data = f.readlines()
                     for i in range(0, len(data), 2):
 
-                        q = data[i].replace('\n', '')
-                        a = data[i + 1].replace('\n', '')
+                        question_raw = data[i].replace('\n', '')
+                        answer_raw = data[i + 1].replace('\n', '')
                         # Test if our removed numbers appear in either the question or answer
-                        x = False
+                        has_removed_number = False
                         # Loop trough all of the regular expressions, check both the question and answer
                         for number in removed_numbers:
-                            q1 = re.findall(number, q)
-                            a1 = re.findall(number, a)
-                            if a1 or q1: # if regex found a match in the question or answer break and change x to True
-                                x = True
+                            question = re.findall(number, question_raw)
+                            answer = re.findall(number, answer_raw)
+                            if answer or question: # if regex found a match in the question or answer break and change x to True
+                                has_removed_number = True
                                 break
-                        if x is True:
+                        if has_removed_number:
                             count_removed += 1
-                            eliminated_data.append([q, a]) # save the eliminated data
+                            eliminated_data.append([question, answer]) # save the eliminated data
                         else:
-                            interim_data.append([q, a]) # Save the "training" data
+                            interim_data.append([question, answer]) # Save the "training" data
 
-    df = pd.DataFrame(interim_data, columns=['Question', 'Answer'])
-    pdf = pd.DataFrame(eliminated_data, columns=['Question', 'Answer'])
-    print(df)
-    print(pdf)
+    print("Writing dataset to CSV")
+    retained_data = pd.DataFrame(interim_data, columns=['Question', 'Answer'])
+    eliminated_data = pd.DataFrame(eliminated_data, columns=['Question', 'Answer'])
+    retained_data.to_csv(SAVED_DATA_PATH+'dataset.csv')
+    eliminated_data.to_csv(SAVED_DATA_PATH+'eliminated_data.csv')
     print("Total removed instances: " + str(count_removed))
+    return retained_data
+
+def get_encoded_data(data):
+    pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from data_preprocessing import get_labeled_data
+from data_preprocessing import reduce_dataset
 import os
 import pandas as pd
 
 if __name__ == '__main__':
-    get_labeled_data()
+    raw_data = reduce_dataset()
+    get_encoded_dataset(raw_data)
