4b488880944bba18739342ffabeaf4d9bbc79388
Pierce Lamb <plamb@visotrust.com>
2022-04-20 07:51:33 -0700

    Added bpe encoding

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,6 +4,13 @@ import re
 import subprocess
 import numpy as np
 from sklearn.model_selection import train_test_split
+import argparse
+import contextlib
+import sys
+from collections import Counter
+from multiprocessing import Pool
+
+from fairseq.data.encoders.gpt2_bpe import get_encoder
 
 DEEPMIND_MATH_PATH = './data/mathematics_dataset-v1.0'
 SAVED_DATA_PATH = './data/'
@@ -15,8 +22,8 @@ RANDOM_STATE = 1337
 def reduce_dataset():
     if os.path.isfile(SAVED_DATA_PATH+RAW_DATASET_NAME):
         print("Detected filtered data locally, returning")
-        retained_data = pd.read_csv(SAVED_DATA_PATH+RAW_DATASET_NAME)
-        return retained_data
+        retained_data = pd.read_csv(SAVED_DATA_PATH+RAW_DATASET_NAME, dtype={"Question": "string", "Answer": "string"})
+        return SAVED_DATA_PATH+RAW_DATASET_NAME, retained_data
 
     removed_numbers_ints = [' 13 ', ' 31 ', ' 82 ', ' 99 ']
     removed_numbers = ["\D1[3]{1}\D", "\D3[1]{1}\D", "\D8[2]{1}\D", "\D9[9]{1}\D"]
@@ -57,28 +64,143 @@ def reduce_dataset():
     retained_data.to_csv(SAVED_DATA_PATH+RAW_DATASET_NAME)
     eliminated_data.to_csv(SAVED_DATA_PATH+'eliminated_data.csv')
     print("Total removed instances: " + str(count_removed))
-    return retained_data
+    return SAVED_DATA_PATH+RAW_DATASET_NAME, retained_data
 
 # https://fairseq.readthedocs.io/en/latest/command_line_tools.html#fairseq-preprocess
-def get_encoded_data(raw_data_filename):
-    print("Encoding the dataset")
-    preprocess_data = subprocess.run([
-        "fairseq-preprocess",
-        "--trainpref=data/raw_dataset.csv",
-        "--destdir=data/encoded_dataset",
-        "--tokenizer=moses",
-        "--bpe=fastbpe"
-    ])
-    print("The exit code was: %d" % preprocess_data.returncode)
+def get_encoded_data(data_splits):
+    has_splits = True
+    for name, split_path in data_splits.items():
+        if not os.path.isfile(split_path):
+            has_splits = False
+            break
 
+    if not has_splits:
+        for name, split_path in data_splits.items():
+            print("\n===Encoding the "+name+" dataset===\n")
+            """
+                Helper script to encode raw text with the GPT-2 BPE using multiple processes.
+                The encoder.json and vocab.bpe files can be obtained here:
+                - https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/encoder.json
+                - https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/vocab.bpe
+                """
+            parser = argparse.ArgumentParser()
+            parser.add_argument(
+                "--encoder-json",
+                help="path to encoder.json",
+                default=SAVED_DATA_PATH+'encoder.json'
+            )
+            parser.add_argument(
+                "--vocab-bpe",
+                type=str,
+                help="path to vocab.bpe",
+                default=SAVED_DATA_PATH + 'vocab.bpe'
+            )
+            parser.add_argument(
+                "--inputs",
+                nargs="+",
+                default=[split_path],
+                help="input files to filter/encode",
+            )
+            parser.add_argument(
+                "--outputs",
+                nargs="+",
+                default=[SAVED_DATA_PATH + name + '.bpe'],
+                help="path to save encoded outputs",
+            )
+            parser.add_argument(
+                "--keep-empty",
+                action="store_true",
+                help="keep empty lines",
+            )
+            parser.add_argument("--workers", type=int, default=20)
+            args = parser.parse_args()
+
+            assert len(args.inputs) == len(
+                args.outputs
+            ), "number of input and output paths should match"
+
+            with contextlib.ExitStack() as stack:
+                inputs = [
+                    stack.enter_context(open(input, "r", encoding="utf-8"))
+                    if input != "-"
+                    else sys.stdin
+                    for input in args.inputs
+                ]
+                outputs = [
+                    stack.enter_context(open(output, "w", encoding="utf-8"))
+                    if output != "-"
+                    else sys.stdout
+                    for output in args.outputs
+                ]
+
+                encoder = MultiprocessingEncoder(args)
+                pool = Pool(args.workers, initializer=encoder.initializer)
+                encoded_lines = pool.imap(encoder.encode_lines, zip(*inputs), 100)
+
+                stats = Counter()
+                for i, (filt, enc_lines) in enumerate(encoded_lines, start=1):
+                    if filt == "PASS":
+                        for enc_line, output_h in zip(enc_lines, outputs):
+                            print(enc_line, file=output_h)
+                    else:
+                        stats["num_filtered_" + filt] += 1
+                    if i % 10000 == 0:
+                        print("processed {} lines".format(i), file=sys.stderr)
+
+                for k, v in stats.most_common():
+                    print("[{}] filtered {} lines".format(k, v), file=sys.stderr)
+
+    else:
+        print("Detected encoded datasets locally")
+
+    return {'train': SAVED_DATA_PATH + 'train.bpe', 'validate': SAVED_DATA_PATH + 'validate.bpe',
+            'test': SAVED_DATA_PATH + 'test.bpe'}
+
+
+class MultiprocessingEncoder(object):
+    def __init__(self, args):
+        self.args = args
+
+    def initializer(self):
+        global bpe
+        bpe = get_encoder(self.args.encoder_json, self.args.vocab_bpe)
+
+    def encode(self, line):
+        global bpe
+        ids = bpe.encode(line)
+        return list(map(str, ids))
+
+    def decode(self, tokens):
+        global bpe
+        return bpe.decode(tokens)
+
+    def encode_lines(self, lines):
+        """
+        Encode a set of lines. All lines will be encoded together.
+        """
+        enc_lines = []
+        for line in lines:
+            line = line.strip()
+            if len(line) == 0 and not self.args.keep_empty:
+                return ["EMPTY", None]
+            tokens = self.encode(line)
+            enc_lines.append(" ".join(tokens))
+        return ["PASS", enc_lines]
+
+    def decode_lines(self, lines):
+        dec_lines = []
+        for line in lines:
+            tokens = map(int, line.strip().split())
+            dec_lines.append(self.decode(tokens))
+        return ["PASS", dec_lines]
 
 # Either loads existing train/valid/test data from disk or creates a
 # 60/20/20 split and saves into numpy files
 def split_data(data_set):
     split_paths = {
-        'train': SAVED_DATA_PATH + 'train.npy',
-        'validate': SAVED_DATA_PATH + 'validate.npy',
-        'test': SAVED_DATA_PATH + 'test.npy',
+        'train': SAVED_DATA_PATH + 'raw_train.csv',
+        'validate': SAVED_DATA_PATH + 'raw_validate.csv',
+        'test': SAVED_DATA_PATH + 'raw_test.csv',
     }
     has_splits = True
     for name, path in split_paths.items():
@@ -87,16 +209,23 @@ def split_data(data_set):
             break
 
     if not has_splits:
-        print("Data splits not detected locally, splitting data and saving")
+        print("Data splits not detected locally, splitting data")
         # Splits the data into 60% train, 20% validate, 20% test
         # From this answer: https://stackoverflow.com/questions/38250710/how-to-split-data-into-3-sets-train-validation-and-test/38251213#38251213
         train, validate, test = np.split(
             data_set.sample(frac=1, random_state=RANDOM_STATE),
             [int(.6*len(data_set)), int(.8*len(data_set))]
         )
-        np.save(SAVED_DATA_PATH +'train',train)
-        np.save(SAVED_DATA_PATH + 'validate', validate)
-        np.save(SAVED_DATA_PATH + 'test', test)
+        print("Saving train csv")
+        train_df = pd.DataFrame(train).drop(columns=['Unnamed: 0'])
+        train_df.to_csv(SAVED_DATA_PATH +'raw_train.csv', header=None, index=None, sep="=")
+        print("Saving validate csv")
+        validate_df = pd.DataFrame(validate).drop(columns=['Unnamed: 0'])
+        validate_df.to_csv(SAVED_DATA_PATH + 'raw_validate.csv', header=None, index=None, sep="=")
+        print("Saving test csv")
+        test_df = pd.DataFrame(test).drop(columns=['Unnamed: 0'])
+        test_df.to_csv(SAVED_DATA_PATH + 'raw_test.csv', header=None, index=None, sep="=")
+
     else:
         print("Data splits detected locally, returning local file paths")
     return split_paths['train'], split_paths['validate'], split_paths['test']
--- main.py
+++ main.py
@@ -13,26 +13,26 @@ if __name__ == '__main__':
     epochs = 10
 
     # data preprocessing
-    raw_data = reduce_dataset()
-    # encoded_data = get_encoded_data(raw_data_filename)
-    train_path, validate_path, test_path = split_data(raw_data)
-
-    # training
-    model_list = get_model_list(FAIRSEQ_MODEL_PATH)
-    for model_path in model_list:
-        print("Training fairseq on: "+model_path)
-        # TODO NOTE THIS IS SETUP TO RUN ON CPU
-        # TODO REMOVE --cpu TO TRAIN ON CUDA
-        training = subprocess.run([
-            "fairseq-train",
-            "--cpu",
-            "--finetune-from-model="+model_path,
-            "--task=language_modeling",
-            "--batch-size="+str(batch_size),
-            "--train-subset="+train_path,
-            "--valid-subset="+validate_path,
-            "--tokenizer=moses",
-            "--bpe=fastbpe",
-            "--lr="+str(learning_rate),
-        ])
-        print("The exit code was: %d" % training.returncode)
+    file_path, raw_data = reduce_dataset()
+    raw_train_path, raw_validate_path, raw_test_path = split_data(raw_data)
+    train_path, validate_path, test_path = get_encoded_data({'train':raw_train_path, 'validate':raw_validate_path, 'test':raw_test_path})
+    #
+    # # training
+    # model_list = get_model_list(FAIRSEQ_MODEL_PATH)
+    # for model_path in model_list:
+    #     print("Training fairseq on: "+model_path)
+    #     # TODO NOTE THIS IS SETUP TO RUN ON CPU
+    #     # TODO REMOVE --cpu TO TRAIN ON CUDA
+    #     training = subprocess.run([
+    #         "fairseq-train",
+    #         "--cpu",
+    #         "--finetune-from-model="+model_path,
+    #         "--task=language_modeling",
+    #         "--batch-size="+str(batch_size),
+    #         "--train-subset="+train_path,
+    #         "--valid-subset="+validate_path,
+    #         "--tokenizer=moses",
+    #         "--bpe=fastbpe",
+    #         "--lr="+str(learning_rate),
+    #     ])
+    #     print("The exit code was: %d" % training.returncode)
