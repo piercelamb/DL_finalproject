dd6b9597cb8bc179cfab00e445e938d543ce29a9
tonyywang <tonyywang@gmail.com>
2022-04-30 01:05:06 -0400

    Last update for queries

--- create_queries.py
+++ create_queries.py
@@ -12,16 +12,16 @@ def create_instances():
     for idx, row in top.iterrows():
         n = row['integer(s)']
         for x in x2:
-            q = 'What is ' + n + ' + ' + str(x) + '?'
-            queries[q] = float(n) + x
+            q = 'Question: What is ' + n + ' + ' + str(x) + '? Answer: '
+            queries[q] = float(float(n) + x)
 
     for n in [31, 82, 99]:
         for x in x2:
-            q = 'What is ' + str(n) + ' + ' + str(x) + '?'
+            q = 'Question: What is ' + str(n) + ' + ' + str(x) + '? Answer: '
             queries[q] = float(n + x)
 
     query_df = pd.DataFrame.from_dict(queries, orient='index').reset_index()
-    query_df = query_df.rename(columns={'index': 'Question', 0: 'Answer'})
+    query_df = query_df.rename(columns={'index': 'Query', 0: 'Answer'})
 
     os.makedirs('./data', exist_ok=True)
     query_df.to_csv('data/queries.csv', index=False)
