90796f0e7ef6ea872f3dd4a26181777835b557d8
Pierce Lamb <plamb@visotrust.com>
2022-05-02 07:59:59 -0700

    few shot

--- eval/evaluate_queries.py
+++ eval/evaluate_queries.py
@@ -7,22 +7,33 @@ from datasets import Features, Value, ClassLabel, Sequence
 TOKENIZER = AutoTokenizer.from_pretrained('gpt2')
 MODEL = AutoModelForCausalLM.from_pretrained("KoboldAI/fairseq-dense-125M")
 
-def tokenize_data(row):
+def tokenize_data(row, idx):
     question = str(row['Query'])
     answer = str(row['Answer'])
-    tokenized = TOKENIZER.encode(question, return_tensors='pt')
-    greedy_output = MODEL.generate(tokenized)
-    print(TOKENIZER.decode(greedy_output[0], skip_special_tokens=True))
+    combined = question + answer
+    if idx % 10 == 0:
+        # we're at a multiple of ten, call generate
+        tokenized = TOKENIZER.encode(question, return_tensors='pt')
+        # pass to the model here
+        greedy_output = MODEL.generate(tokenized)
+
+        print(TOKENIZER.decode(greedy_output[0], skip_special_tokens=True))
+    else:
+        tokenized = TOKENIZER.encode(combined, return_tensors='pt')
+        # pass to the model here
+        # greedy_output = MODEL.generate(tokenized)
+
     return None
 
 
 def main():
     eval_set = load_dataset('csv', data_files='queries_top100_no13.csv')['train']
     # eval set is a Dataset with columns Query and Answer after this
-
+    INDEX = 0
     inputs = eval_set.map(
         tokenize_data,
         remove_columns=eval_set.column_names,
+        with_indices=True
     )
 
 
